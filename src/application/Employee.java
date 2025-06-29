3018260392d4f892ec65de933b68f53b635e5626
Johnathan Goode <89587677+JOHNGOODE16@users.noreply.github.com>
2025-06-29 19:29:33 -0400

    ADD.Employee getters/Manager constructor/Staff constructor/StaffTest

--- src/application/Employee.java
+++ src/application/Employee.java
@@ -1,5 +1,73 @@
 package application;
 
-public class Employee {
+public abstract class Employee {
+	protected static int counterID = 1;
+	protected String firstName, lastName, username, password, department;
+	protected double payRate, taxRate;
+	protected int ptoDays;
+	protected int employeeID;
+	protected int[] hours = new int[7];
+	
+
+	public Employee(String firstName, String lastName, String username, String password, String department,
+					double payRate, double taxRate, int ptoDays) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.username = username;
+		this.password = password;
+		this.department = department;
+		this.payRate = payRate;
+		this.taxRate = taxRate;
+		this.ptoDays = ptoDays;
+		this.employeeID = counterID ++;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public String getDepartment() {
+		return department;
+	}
+	
+	public double getPayRate() {
+		return payRate;
+	}
+	
+	public double getTaxRate() {
+		return taxRate;
+	}
+	
+	public int getPtoDays() {
+		return ptoDays;
+	}
+	
+	public String getEmployeeID() {
+		return String.format("%04d", employeeID);
+	}
+	
+	
+	
+	public void setHours(int day, int hours) {
+		if((day >= 0 && day < 7)&&(hours >=0 && hours <=24)) {
+			this.hours[day] = hours;
+		}
+	}
+	
+	public int[] getHours() {
+		return hours;
+	}
+	
+	public String getFullName() {
+		return firstName + " " + lastName;
+	}
 
 }
---
+++ src/test/StaffTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class StaffTest {
+
+	@Test
+	@DisplayName("Staff Constructor Test:") //implements counterID ++ to generate employeeID. I want to ensure it operates as advertised.
+	void staffTest() {
+		fail("Not yet implemented");
+	}
+
+}
